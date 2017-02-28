f235e20521c19c56ab4defda479e3a8d3e247273
Christian <c.stamariab@gmail.com>
2017-02-28 18:49:31 -0300

    Edicion final del webpack y package.json, agregado live reload para webpack-dev-server

--- app/dist/index.js
+++ app/dist/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
+  constructor(){
+    super();
+    this.nombre = 'Christian Santa Maria'
+  }
+  obtenerNombre(){
+    return "Codigo Facilito";
+  }
   render(){
     return(
-      <h1>Hola Mundo!</h1>
+      <h1>Hola {this.obtenerNombre()}</h1>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./app/dist/index",
+  entry: "./app/dist/index.js",
   module: {
     loaders: [
       {
