c04bb415ee793feedecd74fd689bcf19b5f0ef52
Christian <c.stamariab@gmail.com>
2017-02-28 20:36:40 -0300

    refresh

--- app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import FilterableProductTable from './components/FilterableProductTable.js';
-
-const app = document.getElementById('app');
-ReactDOM.render(
-  <FilterableProductTable/>
-,app);
