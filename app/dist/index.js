7b37c85d82a49a61d1f2b0f798ac511a4bef6f88
Christian <c.stamariab@gmail.com>
2017-02-28 17:53:38 -0300

    Configurando webpack para uso de jsx y generador de bundle y creacion de un Component

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,14 @@
-require('./modulo1.js');
-require('./modulo2.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends React.Component {
+  render(){
+    return(
+      <h1>Hola Mundo!</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
