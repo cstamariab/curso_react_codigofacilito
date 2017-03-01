4e29fb69c0cc98484928326fff2422ff4621cbe8
Christian <c.stamariab@gmail.com>
2017-02-28 23:36:31 -0300

    Se activa checkbox y input con metodos desde el padre

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -2,12 +2,34 @@ import React from 'react';
 export default class SearchBar extends React.Component {
   constructor(){
     super();
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+    this.handleInStock = this.handleInStock.bind(this);
+  }
+  handleFilterTextInput(e) {
+    this.props.onFilter(e.target.value);
+  }
+
+  handleInStock(e) {
+    this.props.onInStock(e.target.checked);
   }
   render(){
     return (
       <div>
-        <input onChange={this.props.onChange} type="text" />
-      <input  type="checkbox"  />
+        <input
+           type="text"
+           placeholder="Buscar"
+           value={this.props.filter}
+           onChange={this.handleFilterTextInput}
+         />
+       <p>
+          <input
+            type="checkbox"
+            checked={this.props.in_stock}
+            onChange={this.handleInStock}
+          />
+          {' '}
+            Only show products in stock
+       </p>
       </div>
     );
   }
