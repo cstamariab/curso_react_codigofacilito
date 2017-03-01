31336015567ab6fd54f50c13a2aeeb7fe14b7cff
Christian <c.stamariab@gmail.com>
2017-02-28 22:33:20 -0300

    Uso de react-router

--- app/src/index.js
+++ app/src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route , IndexRoute, hashHistory} from 'react-router';
 
-import FilterableProductTable from './components/FilterableProductTable.js';
-
-let db = [
-  {id:1,category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {id:2,category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {id:3,category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {id:4,category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {id:5,category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {id:6,category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
-
+import Home from './routes/Home.js';
+import Store from './routes/Store.js';
 
 
 const app = document.getElementById('app');
 ReactDOM.render(
-  <FilterableProductTable store={ db }/>
+  <Router history={hashHistory}>
+    <Route path='/' component={Home} />
+    <Route path='store' component={Store} />
+  </Router>
 ,app);
